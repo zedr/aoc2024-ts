ff362369d3613974534e0dc2ab7f6b2a2e4a11f7
Rigel Di Scala <zedr@zedr.com>
2024-12-04 21:16:13 +0100

    Expand test

--- 3/tests.ts
+++ 3/tests.ts
@@ -3,7 +3,12 @@ import {parseMultipliers} from "./3.ts";
 import type {Pair} from "./3.ts";
 
 Deno.test("can parse one or more expressions as tuples", () => {
-    const s = "mul(6,7)";
-    const result = parseMultipliers(s).next();
+    const s1 = "mul(6,7)";
+    const result = parseMultipliers(s1).next();
     expect(result.value).toEqual([6, 7]);
+
+    const s2 = "mul(6,7)mul(88,0)";
+    const results = parseMultipliers(s2);
+    expect(results.next().value).toEqual([6, 7]);
+    expect(results.next().value).toEqual([88, 0]);
 });
