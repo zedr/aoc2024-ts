aad7b49737219f9f030abbaa79e92fe540ca1a18
Rigel Di Scala <zedr@zedr.com>
2024-12-05 21:41:10 +0100

    Implement second part of ex 3

--- 3/3.ts
+++ 3/3.ts
@@ -1,13 +1,42 @@
 import {readFileLinesAsync} from "../1/1.ts";
-import {isSafe, LINE_MATCHER_RXP, numberStringToArray} from "../2/2.ts";
+
+enum CommandType {Multiply, Do, Dont}
 
 type Pair = [number, number];
 
-const rxp = /mul\((\d+),(\d+)\)/g
+const rxp = /mul\((\d+),(\d+)\)|don't\(\)|do\(\)/g
 
-function* parseMultipliers(s: string): Generator<Pair> {
-    for (const pair of s.matchAll(rxp)) {
-        yield [parseInt(pair[1]), parseInt(pair[2])];
+class Command {
+    type: CommandType;
+    operands: Pair | null;
+
+    constructor(text: string, operands: string[]) {
+        if (text === "do()") {
+            this.type = CommandType.Do;
+            this.operands = null;
+        } else if (text === "don't()") {
+            this.type = CommandType.Dont;
+            this.operands = null;
+        } else if (text.slice(0, 4) === "mul(") {
+            this.type = CommandType.Multiply;
+            this.operands = [parseInt(operands[0]), parseInt(operands[1])];
+        } else {
+            throw ("Unsupported command text");
+        }
+    }
+
+    execute(): number {
+        if ((this.type === CommandType.Multiply) && this.operands !== null) {
+            return this.operands[0] * this.operands[1];
+        } else {
+            throw (`Cannot execute command: ${this.type.toString()}`)
+        }
+    }
+}
+
+function* parseCommands(s: string): Generator<Command> {
+    for (const matched of s.matchAll(rxp)) {
+        yield new Command(matched[0], matched.slice(1));
     }
 }
 
@@ -25,15 +54,26 @@ async function main(): Promise<void> {
     }
 
     // iterate through the lines and count the safe ones
+    let isEnabled = true;
     for await(const line of await readFileLinesAsync(file)) {
-        for (const pair of parseMultipliers(line)) {
-            sumOfResults += pair[0] * pair[1];
+        for (const command of parseCommands(line)) {
+            if (command.type === CommandType.Multiply) {
+                if (isEnabled) {
+                    sumOfResults += command.execute();
+                }
+            } else if (command.type === CommandType.Do) {
+                isEnabled = true;
+            } else if (command.type === CommandType.Dont) {
+                isEnabled = false;
+            } else {
+                throw ("Unsupported command");
+            }
         }
     }
     console.log(sumOfResults);
 }
 
-main();
+if (import.meta.main) main();
 
-export {parseMultipliers};
+export {parseCommands, Command, CommandType};
 export type {Pair};
--- 3/tests.ts
+++ 3/tests.ts
@@ -1,14 +1,25 @@
 import {expect} from "jsr:@std/expect";
-import {parseMultipliers} from "./3.ts";
-import type {Pair} from "./3.ts";
+import {parseCommands, Command, CommandType} from "./3.ts";
 
 Deno.test("can parse one or more expressions as tuples", () => {
     const s1 = "mul(6,7)";
-    const result = parseMultipliers(s1).next();
-    expect(result.value).toEqual([6, 7]);
+    const result = parseCommands(s1).next();
+    expect(result.value.operands).toEqual([6, 7]);
 
     const s2 = "mul(6,7)mul(88,0)";
-    const results = parseMultipliers(s2);
-    expect(results.next().value).toEqual([6, 7]);
-    expect(results.next().value).toEqual([88, 0]);
+    const results = parseCommands(s2);
+    expect(results.next().value.operands).toEqual([6, 7]);
+    expect(results.next().value.operands).toEqual([88, 0]);
+});
+
+Deno.test("all three commands can be parse from text", () => {
+    const cmd = new Command("mul(4,2)", ["4", "2"]);
+    expect(cmd.type).toBe(CommandType.Multiply);
+    expect(cmd.operands).toEqual([4, 2]);
+
+    const cmd2 = new Command("do()", ["4", "2"]);
+    expect(cmd2.type).toBe(CommandType.Do);
+
+    const cmd3 = new Command("don't()", ["4", "2"]);
+    expect(cmd3.type).toBe(CommandType.Dont);
 });
